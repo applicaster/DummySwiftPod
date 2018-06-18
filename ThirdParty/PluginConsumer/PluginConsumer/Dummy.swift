a1e2c23c9f1c8fb601f61ae864b4769fa73e4d01
Ilan Levy <i.levy@applicaster.com>
2018-06-18 19:15:07 +0300

    no message

---
+++ ThirdParty/PluginConsumer/PluginConsumer/Dummy.swift
@@ -0,0 +1,14 @@
+//
+//  Dummry.swift
+//  PluginConsumer
+//
+//  Created by IlanL on 18/06/2018.
+//  Copyright © 2018 Applicaster Ltd. All rights reserved.
+//
+
+import Foundation
+import EmptySwiftPod
+
+class Dummy {
+    
+}
